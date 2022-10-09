d0bdca438a2e79873c90f10f48befd26e11ead14
Clint <clintegargo@protonmail.ch>
2022-10-09 17:00:00 +0800

    Added Button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+	buttonColoured: {
+		backgroundColor: "#CC4D46",
+		textAlign: 'center',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 12,
+		width: 320,
+		height: 64,
+		margin: 12,
+	},
+
+	buttonMono: {
+		backgroundColor: '#EBECEF',
+		textAlign: 'center',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 12,
+		width: 320,
+		height: 64,
+		margin: 12,
+	},
+
+	textLight: {
+		color: '#FFFFFF',
+		fontFamily: 'Ubuntu-Regular',
+	},
+
+	textDark: {
+		color: '#1E1E1E',
+		fontFamily: 'Ubuntu-Regular',
+	}
+});
+
+
+export default styles;
