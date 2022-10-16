46c82d052ba2dc60ebb5c2d51d0144fcc4096ef6
Clint <clintegargo@protonmail.ch>
2022-10-16 12:39:08 +0800

    Added DataPrivacy Screen

---
+++ src/screens/Auth/DataPrivacy/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+
+	contents: {
+		flex: 1,
+		backgroundColor: '#FFFFFF',
+		textAlign: 'left',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	buttonContainer: {
+		backgroundColor: '#FFFFFF',
+		textAlign: 'center',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+	},
+});
+
+
+export default styles;
