369937df5e4a7e543914b0ff37f7ee0226e78f64
Clint <clintegargo@protonmail.ch>
2022-10-31 16:37:44 +0800

    Added AnnouncememntUniversity screen

---
+++ src/screens/Dashboard/Announcements/University/index.js
@@ -0,0 +1,16 @@
+import { View, ScrollView, Text } from 'react-native';
+import { Announcement } from '../../../../components/Announcements';
+import styles from './styles';
+
+
+const AnnouncementUniversity = (props) => {
+	return(
+		<View style={styles.container}>
+			<ScrollView>
+			</ScrollView>
+		</View>
+	);
+}
+
+
+export default AnnouncementUniversity;
