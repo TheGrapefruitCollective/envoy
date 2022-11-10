a451711ae2721fb087d85c85fd9361fcfa95a25e
Clint Egargo <clintegargo@gmail.com>
2022-11-10 12:34:21 +0800

    Formatted with Prettier

--- src/screens/Dashboard/Announcements/Department/index.js
+++ src/screens/Dashboard/Announcements/Department/index.js
@@ -1,16 +1,13 @@
-import { View, ScrollView, Text } from 'react-native';
-import { Announcement } from '../../../../components/Announcements';
-import styles from './styles';
-
+import { View, ScrollView, Text } from "react-native";
+import { Announcement } from "../../../../components/Announcements";
+import styles from "./styles";
 
 function AnnouncementDepartment(props) {
 	return (
 		<View style={styles.container}>
-			<ScrollView>
-			</ScrollView>
+			<ScrollView></ScrollView>
 		</View>
 	);
 }
-
 
 export default AnnouncementDepartment;
