ee2c7d6f918d053dc495f7e0e3a67acd6407280a
Clint Egargo <clintegargo@gmail.com>
2022-11-10 13:46:16 +0800

    Formatted Home screen with Prettier

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,29 +1,29 @@
-import { View } from "react-native";
-import Logo from "../../components/Logo";
-import { ButtonBlack } from "../../components/Button";
+import { View } from 'react-native';
+import Logo from '../../components/Logo';
+import { ButtonBlack } from '../../components/Button';
 
-import styles from "./styles";
+import styles from './styles';
 
 function Home(props) {
-	return (
-		<View style={styles.container}>
-			<Logo />
+  return (
+    <View style={styles.container}>
+      <Logo />
 
-			<ButtonBlack
-				title="Create Account"
-				onPress={() => {
-					props.navigation.navigate("CreateAccountStepOne");
-				}}
-			/>
+      <ButtonBlack
+        title='Create Account'
+        onPress={() => {
+          props.navigation.navigate('CreateAccountStepOne');
+        }}
+      />
 
-			<ButtonBlack
-				title="Login"
-				onPress={() => {
-					props.navigation.navigate("LogIn");
-				}}
-			/>
-		</View>
-	);
+      <ButtonBlack
+        title='Login'
+        onPress={() => {
+          props.navigation.navigate('LogIn');
+        }}
+      />
+    </View>
+  );
 }
 
 export default Home;
