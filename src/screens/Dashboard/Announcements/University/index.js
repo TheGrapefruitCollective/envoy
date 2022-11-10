9f9117e8344259407d65e651237b7f13f07253a1
Clint Egargo <clintegargo@gmail.com>
2022-11-10 13:45:54 +0800

    Formatted Announcements/University screen with Prettier

--- src/screens/Dashboard/Announcements/University/index.js
+++ src/screens/Dashboard/Announcements/University/index.js
@@ -1,13 +1,13 @@
-import { View, ScrollView, Text } from "react-native";
-import { Announcement } from "../../../../components/Announcements";
-import styles from "./styles";
+import { View, ScrollView, Text } from 'react-native';
+import { Announcement } from '../../../../components/Announcements';
+import styles from './styles';
 
 function AnnouncementUniversity(props) {
-	return (
-		<View style={styles.container}>
-			<ScrollView></ScrollView>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <ScrollView></ScrollView>
+    </View>
+  );
 }
 
 export default AnnouncementUniversity;
