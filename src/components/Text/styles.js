40319f5d3e0c212fa7d4842114ea75c7e0134397
Clint Egargo <clintegargo@gmail.com>
2022-11-10 13:43:26 +0800

    Formatted Text component with Prettier

--- src/components/Text/styles.js
+++ src/components/Text/styles.js
@@ -1,26 +1,26 @@
-import { StyleSheet } from "react-native";
-import Colours from "../Colours";
+import { StyleSheet } from 'react-native';
+import Colours from '../Colours';
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		textAlign: "center",
-		alignContent: "center",
-		margin: 12,
-		backgroundColor: Colours.colour0,
-	},
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    alignContent: 'center',
+    margin: 12,
+    backgroundColor: Colours.colour0,
+  },
 
-	regular: {
-		color: Colours.colour6,
-		fontFamily: "Ubuntu-Regular",
-		fontSize: 12,
-	},
+  regular: {
+    color: Colours.colour6,
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 12,
+  },
 
-	bold: {
-		color: Colours.colour6,
-		fontFamily: "Ubuntu-Bold",
-		fontSize: 24,
-	},
+  bold: {
+    color: Colours.colour6,
+    fontFamily: 'Ubuntu-Bold',
+    fontSize: 24,
+  },
 });
 
 export default styles;
