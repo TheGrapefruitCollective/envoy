aee1a59cc74cedd7d3e0c53b25a3c5518d925520
Clint Egargo <clintegargo@gmail.com>
2022-11-10 13:44:10 +0800

    Formatted CreateAccount screen with Prettier

--- src/screens/Auth/CreateAccount/index.js
+++ src/screens/Auth/CreateAccount/index.js
@@ -1,180 +1,170 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
-	Keyboard,
-	KeyboardAvoidingView,
-	TouchableWithoutFeedback,
-	View,
-	ScrollView,
-	Platform,
-} from "react-native";
-import InputField from "../../../components/InputField";
-import { ButtonBlack } from "../../../components/Button";
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  View,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import InputField from '../../../components/InputField';
+import { ButtonBlack } from '../../../components/Button';
 import {
-	validateEmail,
-	validatePassword,
-	validateCollege,
-	validateConfirmPassword,
-} from "./script";
-import styles from "./styles";
+  validateEmail,
+  validatePassword,
+  validateCollege,
+  validateConfirmPassword,
+} from './script';
+import styles from './styles';
 
 export function CreateAccountStepOne(props) {
-	const [fullName, setFullName] = useState("");
-	const [college, setCollege] = useState("");
-	const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState('');
+  const [college, setCollege] = useState('');
+  const [email, setEmail] = useState('');
 
-	return (
-		<>
-			<KeyboardAvoidingView
-				enabled
-				behavior={Platform.OS === "ios" ? "padding" : null}
-				style={styles.container}
-			>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<>
-						<View style={styles.inputFieldContainer}>
-							<ScrollView
-								showsVerticalScrollIndicator={false}
-								contentContainerStyle={{ paddingVertical: 12 }}
-								keyboardShouldPersistTaps="always"
-							>
-								<InputField
-									placeholder="Enter your first name"
-									onChangeText={(fullName) => {
-										setFullName(fullName);
-									}}
-								/>
-								<InputField
-									placeholder="Enter your college department"
-									onChangeText={(college) => {
-										setCollege(college);
-										//validateCollege(college)
-									}}
-									result={
-										validateCollege(college) === true
-											? null
-											: "Invalid college department."
-									}
-								/>
-								<InputField
-									placeholder="Enter your email address"
-									onChangeText={(email) => {
-										setEmail(email);
-									}}
-									result={
-										validateEmail(email) === true
-											? null
-											: "Invalid email address."
-									}
-								/>
-							</ScrollView>
-						</View>
-					</>
-				</TouchableWithoutFeedback>
-			</KeyboardAvoidingView>
-			<View>
-				<View style={styles.buttonContainer}>
-					<ButtonBlack
-						title="Continue"
-						disabled={
-							//((fullName && college) !== '') ? false : true
-							//(fullName !== '' && validateCollege(college) === true) ? false : true
-							email !== "" &&
-							fullName !== "" &&
-							college !== "" &&
-							validateCollege(college) === true &&
-							validateEmail(email)
-								? false
-								: true
-						}
-						onPress={() =>
-							props.navigation.navigate("CreateAccountStepTwo")
-						}
-					/>
-				</View>
-			</View>
-		</>
-	);
+  return (
+    <>
+      <KeyboardAvoidingView
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={styles.container}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <View style={styles.inputFieldContainer}>
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingVertical: 12 }}
+                keyboardShouldPersistTaps='always'
+              >
+                <InputField
+                  placeholder='Enter your first name'
+                  onChangeText={(fullName) => {
+                    setFullName(fullName);
+                  }}
+                />
+                <InputField
+                  placeholder='Enter your college department'
+                  onChangeText={(college) => {
+                    setCollege(college);
+                    //validateCollege(college)
+                  }}
+                  result={
+                    validateCollege(college) === true
+                      ? null
+                      : 'Invalid college department.'
+                  }
+                />
+                <InputField
+                  placeholder='Enter your email address'
+                  onChangeText={(email) => {
+                    setEmail(email);
+                  }}
+                  result={
+                    validateEmail(email) === true
+                      ? null
+                      : 'Invalid email address.'
+                  }
+                />
+              </ScrollView>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+      <View>
+        <View style={styles.buttonContainer}>
+          <ButtonBlack
+            title='Continue'
+            disabled={
+              //((fullName && college) !== '') ? false : true
+              //(fullName !== '' && validateCollege(college) === true) ? false : true
+              email !== '' &&
+              fullName !== '' &&
+              college !== '' &&
+              validateCollege(college) === true &&
+              validateEmail(email)
+                ? false
+                : true
+            }
+            onPress={() => props.navigation.navigate('CreateAccountStepTwo')}
+          />
+        </View>
+      </View>
+    </>
+  );
 }
 
 export const CreateAccountStepTwo = (props) => {
-	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-	return (
-		<>
-			<KeyboardAvoidingView
-				enabled
-				behavior={Platform.OS === "ios" ? "padding" : null}
-				style={styles.container}
-			>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<>
-						<View style={styles.inputFieldContainer}>
-							<ScrollView
-								showsVerticalScrollIndicator={false}
-								contentContainerStyle={{ paddingVertical: 12 }}
-								keyboardShouldPersistTaps="always"
-							>
-								<InputField
-									label="Password"
-									placeholder="Enter your password"
-									secureTextEntry={true}
-									onChangeText={(password) => {
-										setPassword(password);
-									}}
-									result={
-										validatePassword(password) === true
-											? null
-											: "Password must be case-sensitive alphanumeric and contain special characters."
-									}
-								/>
+  return (
+    <>
+      <KeyboardAvoidingView
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={styles.container}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <View style={styles.inputFieldContainer}>
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingVertical: 12 }}
+                keyboardShouldPersistTaps='always'
+              >
+                <InputField
+                  label='Password'
+                  placeholder='Enter your password'
+                  secureTextEntry={true}
+                  onChangeText={(password) => {
+                    setPassword(password);
+                  }}
+                  result={
+                    validatePassword(password) === true
+                      ? null
+                      : 'Password must be case-sensitive alphanumeric and contain special characters.'
+                  }
+                />
 
-								<InputField
-									label="Confirm Password"
-									placeholder="Confirm password"
-									secureTextEntry={true}
-									onChangeText={(confirmPassword) => {
-										setConfirmPassword(confirmPassword);
-									}}
-									result={
-										validateConfirmPassword(
-											password,
-											confirmPassword
-										) === true
-											? null
-											: "Password does not match."
-									}
-								/>
+                <InputField
+                  label='Confirm Password'
+                  placeholder='Confirm password'
+                  secureTextEntry={true}
+                  onChangeText={(confirmPassword) => {
+                    setConfirmPassword(confirmPassword);
+                  }}
+                  result={
+                    validateConfirmPassword(password, confirmPassword) === true
+                      ? null
+                      : 'Password does not match.'
+                  }
+                />
 
-								<View
-									style={{ flex: 1, flexDirection: "row" }}
-								></View>
-							</ScrollView>
-						</View>
-					</>
-				</TouchableWithoutFeedback>
-			</KeyboardAvoidingView>
-			<View>
-				<View style={styles.buttonContainer}>
-					<ButtonBlack
-						title="Create account"
-						disabled={
-							password !== "" &&
-							confirmPassword !== "" &&
-							validatePassword(password) === true &&
-							validateConfirmPassword(
-								password,
-								confirmPassword
-							) === true
-								? false
-								: true
-						}
-						// onPress={
-						// 	() => props.navigation.navigate('DataPrivacy')
-						// }
-					/>
-				</View>
-			</View>
-		</>
-	);
+                <View style={{ flex: 1, flexDirection: 'row' }}></View>
+              </ScrollView>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+      <View>
+        <View style={styles.buttonContainer}>
+          <ButtonBlack
+            title='Create account'
+            disabled={
+              password !== '' &&
+              confirmPassword !== '' &&
+              validatePassword(password) === true &&
+              validateConfirmPassword(password, confirmPassword) === true
+                ? false
+                : true
+            }
+            // onPress={
+            // 	() => props.navigation.navigate('DataPrivacy')
+            // }
+          />
+        </View>
+      </View>
+    </>
+  );
 };
--- src/screens/Auth/CreateAccount/script.js
+++ src/screens/Auth/CreateAccount/script.js
@@ -1,64 +1,64 @@
 export const validateCollege = (college) => {
-	const COLLEGES = [
-		"",
-		"CEA",
-		"COLLEGE OF ENGINEERING AND ARCHITECTURE",
-		"CITC",
-		"COLLEGE OF INFORMATION TECHNOLOGY AND COMPUTING",
-		"CSM",
-		"COLLEGE OF SCIENCE AND MATHEMATICS",
-		"CSTE",
-		"COLLEGE OF SCIENCE AND TECHNOLOGY EDUCATION",
-		"COT",
-		"COLLEGE OF TECHNOLOGY",
-	];
+  const COLLEGES = [
+    '',
+    'CEA',
+    'COLLEGE OF ENGINEERING AND ARCHITECTURE',
+    'CITC',
+    'COLLEGE OF INFORMATION TECHNOLOGY AND COMPUTING',
+    'CSM',
+    'COLLEGE OF SCIENCE AND MATHEMATICS',
+    'CSTE',
+    'COLLEGE OF SCIENCE AND TECHNOLOGY EDUCATION',
+    'COT',
+    'COLLEGE OF TECHNOLOGY',
+  ];
 
-	if (COLLEGES.includes(college.toUpperCase()) === true) {
-		return true;
-	} else {
-		return false;
-	}
+  if (COLLEGES.includes(college.toUpperCase()) === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const validateEmail = (email) => {
-	if (email === "") {
-		return true;
-	}
-	const VALID_EMAIL = new RegExp(
-		"([a-zA-Z0-9._]{4,16})+[@]+([a-zA-Z0-9]{2,6})+[.]+[a-zA-Z0-9]{2,6}"
-	);
+  if (email === '') {
+    return true;
+  }
+  const VALID_EMAIL = new RegExp(
+    '([a-zA-Z0-9._]{4,16})+[@]+([a-zA-Z0-9]{2,6})+[.]+[a-zA-Z0-9]{2,6}'
+  );
 
-	if (email.search(VALID_EMAIL) === 0) {
-		return true;
-	} else {
-		return false;
-	}
+  if (email.search(VALID_EMAIL) === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const validatePassword = (password) => {
-	if (password === "") {
-		return true;
-	}
+  if (password === '') {
+    return true;
+  }
 
-	const VALID_PASSWORD = new RegExp(
-		"(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]){8,16}"
-	);
+  const VALID_PASSWORD = new RegExp(
+    '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]){8,16}'
+  );
 
-	if (password.search(VALID_PASSWORD) === 0) {
-		return true;
-	} else {
-		return false;
-	}
+  if (password.search(VALID_PASSWORD) === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const validateConfirmPassword = (password, confirmPassword) => {
-	if (confirmPassword === "") {
-		return true;
-	}
+  if (confirmPassword === '') {
+    return true;
+  }
 
-	if (password === confirmPassword) {
-		return true;
-	} else {
-		return false;
-	}
+  if (password === confirmPassword) {
+    return true;
+  } else {
+    return false;
+  }
 };
