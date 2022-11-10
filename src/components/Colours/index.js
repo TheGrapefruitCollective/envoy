4d28c0bb7c5a1434df2d2a6458996491855dd97e
Clint Egargo <clintegargo@gmail.com>
2022-11-10 12:32:08 +0800

    Formatted with Prettier

--- src/components/Colours/index.js
+++ src/components/Colours/index.js
@@ -1,11 +1,9 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet } from "react-native";
 
 const Colours = StyleSheet.create({
-	black: '#0F0F0F',
-	white: '#FFFFFF',
-	red  : '#E1645B',
+	black: "#0F0F0F",
+	white: "#FFFFFF",
+	red: "#E1645B",
 });
-
 
 export default Colours;
