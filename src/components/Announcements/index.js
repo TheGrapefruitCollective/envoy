670aaf3f2d818ca1167b5dbec2fe14217aa04819
Clint Egargo <clintegargo@gmail.com>
2022-11-10 13:41:52 +0800

    Formatted Announcement component with Prettier

--- src/components/Announcements/index.js
+++ src/components/Announcements/index.js
@@ -1,20 +1,18 @@
-import { View } from "react-native";
-import { Card, Title, Paragraph } from "react-native-paper";
-import styles from "./styles";
+import { View } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import styles from './styles';
 
 export function Announcement(props) {
-	return (
-		<View style={styles.container}>
-			<Card style={styles.card}>
-				<Card.Content>
-					<Title style={styles.text}>{props.title}</Title>
-				</Card.Content>
-				<Card.Content>
-					<Paragraph style={styles.text}>
-						{props.description}
-					</Paragraph>
-				</Card.Content>
-			</Card>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Title style={styles.text}>{props.title}</Title>
+        </Card.Content>
+        <Card.Content>
+          <Paragraph style={styles.text}>{props.description}</Paragraph>
+        </Card.Content>
+      </Card>
+    </View>
+  );
 }
--- src/components/Announcements/styles.js
+++ src/components/Announcements/styles.js
@@ -1,25 +1,25 @@
-import { StyleSheet } from "react-native";
-import Colours from "../../components/Colours";
+import { StyleSheet } from 'react-native';
+import Colours from '../../components/Colours';
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignContent: "center",
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: Colours.colour0,
-	},
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colours.colour0,
+  },
 
-	card: {
-		backgroundColor: Colours.colour8,
-		borderRadius: 12,
-		margin: 12,
-		width: 320,
-	},
+  card: {
+    backgroundColor: Colours.colour8,
+    borderRadius: 12,
+    margin: 12,
+    width: 320,
+  },
 
-	text: {
-		color: Colours.colour6,
-	},
+  text: {
+    color: Colours.colour6,
+  },
 });
 
 export default styles;
