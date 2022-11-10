d030ab773a190e0264348103b3a3dffb219e38cf
Clint Egargo <clintegargo@gmail.com>
2022-11-10 12:33:44 +0800

    Formatted with Prettier

--- src/screens/Auth/DataPrivacy/index.js
+++ src/screens/Auth/DataPrivacy/index.js
@@ -1,28 +1,25 @@
-import { View, ScrollView } from 'react-native';
-import { TextBold, TextRegular } from '../../../components/Text';
-import styles from './styles';
-
+import { View, ScrollView } from "react-native";
+import { TextBold, TextRegular } from "../../../components/Text";
+import styles from "./styles";
 
 export function TermsOfService(props) {
 	return (
 		<View style={styles.container}>
 			<ScrollView>
-				<TextBold title='Terms of Service' />
-				<TextRegular title='Terms of Service' />
+				<TextBold title="Terms of Service" />
+				<TextRegular title="Terms of Service" />
 			</ScrollView>
 		</View>
 	);
 }
-
 
 export function PrivacyPolicy(props) {
 	return (
 		<View style={styles.container}>
 			<ScrollView>
-				<TextBold size={24} title='Privacy Policy' />
-				<TextRegular title='Privacy Policy' />
+				<TextBold size={24} title="Privacy Policy" />
+				<TextRegular title="Privacy Policy" />
 			</ScrollView>
 		</View>
 	);
 }
-
