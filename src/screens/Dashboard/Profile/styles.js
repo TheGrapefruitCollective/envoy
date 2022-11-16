13f35576682028cc95501bd765f80f04174b67aa
Clint Egargo <clintegargo@gmail.com>
2022-11-16 10:42:50 +0800

    Added Profile screen

---
+++ src/screens/Dashboard/Profile/styles.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) TheGrapefruitCollective
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { StyleSheet } from 'react-native';
+import Colours from '../../../components/Colours';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: Colours.white,
+  },
+
+  profilePreview: {},
+
+  inputFieldContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  buttonContainer: {
+    backgroundColor: Colours.white,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
+
+export default styles;
