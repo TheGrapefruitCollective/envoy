797121f51db24252c084a539a6c0a3794da71e98
cegargo <clintegargo@protonmail.ch>
2022-11-16 14:28:58 +0800

    Added app about info and views

--- src/screens/Dashboard/Menu/index.js
+++ src/screens/Dashboard/Menu/index.js
@@ -5,30 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  ScrollView,
-  Platform,
-  TouchableWithoutFeedback,
-  View,
-  Text,
-} from 'react-native';
-import Logo from '../../../components/Logo';
+import { View } from 'react-native';
+import { LogoMini } from '../../../components/Logo';
+import { TextRegular } from '../../../components/Text';
 import { ButtonBlack, ButtonRed } from '../../../components/Button';
-import InputField from '../../../components/InputField';
-
 import styles from './styles';
 
 function Menu() {
   return (
-    <View style={styles.container}>
-      <ButtonBlack title='License' />
-      <ButtonBlack title='GitHub' />
-      <ButtonBlack title='Terms of Service' />
-      <ButtonBlack title='Privacy Policy' />
-      <ButtonRed title='Delete account' />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.about}>
+          <LogoMini />
+          <View style={styles.aboutInfo}>
+            <TextRegular title='Envoy' />
+            <TextRegular title='(c) The Grapefruit Collective' />
+            <TextRegular title='Version 1.0.0' />
+          </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <ButtonBlack title='License' />
+          <ButtonBlack title='GitHub' />
+          <ButtonBlack title='Terms of Service' />
+          <ButtonBlack title='Privacy Policy' />
+        </View>
+        <View style={styles.buttonDeleteContainer}>
+          <ButtonRed title='Delete account' />
+        </View>
+      </View>
+    </>
   );
 }
 
