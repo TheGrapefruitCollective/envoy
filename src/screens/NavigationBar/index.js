0bdb3e423428708da77aa66e281e775cf6e1556e
cegargo <clintegargo@protonmail.ch>
2022-11-16 11:34:01 +0800

    Addeded MaterialCommunityIcons and Colours

--- src/screens/NavigationBar/index.js
+++ src/screens/NavigationBar/index.js
@@ -7,16 +7,40 @@
 
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import AnnouncementHome from '../Dashboard/Announcements/AnnouncementHome';
 import Profile from '../Dashboard/Profile';
+import Colours from '../../components/Colours';
 
 const Tab = createMaterialBottomTabNavigator();
 
 function NavigationBar() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Announcements' component={AnnouncementHome} />
-      <Tab.Screen name='Profile' component={Profile} />
+    <Tab.Navigator
+      initialRouteName='Announcements'
+      activeColor={Colours.white}
+      barStyle={{ backgroundColor: Colours.black, height: 86 }}
+    >
+      <Tab.Screen
+        name='Announcements'
+        component={AnnouncementHome}
+        options={{
+          tabBarLabel: 'Announcements',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='card-text' color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='account' color={color} size={24} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
