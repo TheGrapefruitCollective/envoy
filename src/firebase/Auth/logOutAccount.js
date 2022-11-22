14d6248141e58cc0d0f9e62ca7ae175dba4d8210
Clint Egargo <clintegargo@gmail.com>
2022-11-22 15:42:08 +0800

    Renamed src/firebase/scripts to src/firebase/Auth

---
+++ src/firebase/Auth/logOutAccount.js
@@ -0,0 +1,15 @@
+import { auth } from '../config';
+import { signOut } from 'firebase/auth';
+
+const logOutAccount = ({ navigation }) => {
+  signOut(auth)
+    .then(() => {
+      console.log('Logged out.');
+      navigation.navigate('Home');
+    })
+    .catch((error) => {
+      console.error(`Error logging out: ${error}`);
+    });
+};
+
+export default logOutAccount;
