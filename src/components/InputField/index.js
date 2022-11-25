28b0e1b75dafef21b51a967ac0f9e0bd9b6de104
Clint Egargo <clintegargo@gmail.com>
2022-11-25 17:26:10 +0800

    Updated: added result props

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -13,13 +13,12 @@ function InputField(props) {
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.inputField}
+        style={[styles.inputField, props.result === true ? styles.error : null]}
         placeholder={props.placeholder}
         placeholderTextColor={Colours.colour4}
         secureTextEntry={props.secureTextEntry}
         onChangeText={props.onChangeText}
       />
-      <Text style={styles.result}>{props.result}</Text>
     </View>
   );
 }
